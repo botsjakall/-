87dcac2c512d051126ae5284a707772eb3436bed
botsjakall <48436905+botsjakall@users.noreply.github.com>
2019-03-15 14:57:53 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,59 @@
+
+    
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+const { Client, Util } = require('discord.js');  
+const getYoutubeID = require('get-youtube-id'); 
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const request = require('request');
+const queue = new Map(); 
+const client = new Discord.Client(); 
+const db = require('quick.db');
+const giphy = require('giphy-api')();    
+const googl = require('goo.gl'); 
+const translate = require('google-translate-api');   
+const fs = require("fs"); 
+var data = JSON.parse(fs.readFileSync('data.json','utf8'))
+const moment = require("moment");
+const UserBlocked = new Set(); 
+const jimp = require('jimp');   
+const math = require('math-expression-evaluator'); 
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it'); 
+const zalgo = require('zalgolize');   
+const sql = require("sqlite");
+ const dateFormat = require('dateformat'); 
+ const pretty = require('pretty-ms') 
+,ti={}  
+,spee={};
+ const prefix = "*";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
